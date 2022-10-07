b24ecaae7d6fda0d13a4e4693b64eb5c7079a1f0
Diana Anton <diana.anton00@e-uvt.ro>
2022-10-07 14:28:19 +0300

    Updated theme adn style, waiting for content.

--- web/dist/js/scripts.js
+++ web/dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - New Age v6.0.6 (https://startbootstrap.com/theme/new-age)
+* Start Bootstrap - The SPOT v6.0.6 (https://startbootstrap.com/theme/new-age)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/the-spot-web/blob/master/LICENSE)
 */
 //
 // Scripts
--- web/scripts/start.js
+++ web/scripts/start.js
@@ -1,24 +1,56 @@
-const concurrently = require('concurrently');
-const upath = require('upath');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
+const concurrently = require("concurrently");
+const upath = require("upath");
 
-concurrently([
-    { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
-    { 
-        command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
-        name: 'SB_BROWSER_SYNC', 
-        prefixColor: 'bgGreen.bold',
+const browserSyncPath = upath.resolve(
+    upath.dirname(__filename),
+    "../node_modules/.bin/browser-sync"
+);
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDMjy-ED6azv1ynnvtBtfUkDS-fssC2SMk",
+    authDomain: "the-spot-web.firebaseapp.com",
+    projectId: "the-spot-web",
+    storageBucket: "the-spot-web.appspot.com",
+    messagingSenderId: "349860846797",
+    appId: "1:349860846797:web:afe6f95084c7de0d4c5d84",
+    measurementId: "G-XJ5L70SHGK",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+concurrently(
+    [
+        {
+            command: "node scripts/sb-watch.js",
+            name: "SB_WATCH",
+            prefixColor: "bgBlue.bold",
+        },
+        {
+            command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
+            name: "SB_BROWSER_SYNC",
+            prefixColor: "bgGreen.bold",
+        },
+    ],
+    {
+        prefix: "name",
+        killOthers: ["failure", "success"],
     }
-], {
-    prefix: 'name',
-    killOthers: ['failure', 'success'],
-}).then(success, failure);
+).then(success, failure);
 
 function success() {
-    console.log('Success');    
+    console.log("Success");
 }
 
 function failure() {
-    console.log('Failure');
+    console.log("Failure");
 }
