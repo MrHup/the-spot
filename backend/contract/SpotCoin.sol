5320a566e729e0eeab0eae74c859210f99cb3773
Diana Anton <diana.anton00@e-uvt.ro>
2022-10-08 20:54:51 +0300

    Added more functions

--- backend/contract/SpotCoin.sol
+++ backend/contract/SpotCoin.sol
@@ -4,17 +4,18 @@ pragma solidity ^0.8.10;
 contract SpotCoin {
     int spot_pool = 50000000;
     address owner;
-
+    
     constructor() {
         owner = msg.sender;
     }
-
+    
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
     //----------------------------------------------------------------------------------------------------------------//
-    enum TransactionType { exchange, received, sent, spot_creation }
+    // 1. SpotCoin
+    enum TransactionType { exchange, received, sent, spot_creation, spot_purchase, spot_sale }
     
     struct Transaction {
         uint timestamp;
@@ -45,6 +46,7 @@ contract SpotCoin {
         string image_uri;
     }
     //----------------------------------------------------------------------------------------------------------------//
+    // Mapping
     User[] users;
     mapping(string => uint) private pv_users_map;
     mapping(string => uint) private pb_users_map;
@@ -54,7 +56,6 @@ contract SpotCoin {
     /**
                                         USER-RELATED FUNCTIONS
     */
-
     /**
      * Used to check if a user exists by using the private key.
      */
@@ -63,7 +64,6 @@ contract SpotCoin {
             return true;
         else return false;
     }
-
     /**
      * Used to check if a user exists by using the public key.
      */
@@ -72,22 +72,24 @@ contract SpotCoin {
             return true;
         else return false;
     }
-
     /**
      * Used only for the creation of a new user to check if both private and public keys are unique.
      */
     function check_user_unique(string memory private_key, string memory public_key) private view returns (bool){
         return check_user_exists_pv(private_key) && check_user_exists_pb(public_key);
     }
-    
+    /**
+     * Used to create a new user.
+     */
     function get_user_by_public_key(string memory public_key) public view returns (User memory) {
+        // check if usr exists and  return user
         if (check_user_exists_pb(public_key))
-        {
-            return users[pb_users_map[public_key]];
-        }
-        else revert ("User does not exist");
+            return users[pb_users_map[public_key] - 1];
+        else revert("User does not exist");
     }
-
+    /**
+     * Used to create a new user.
+     */
     function get_user_by_private_key(string memory private_key) private view returns (User memory) {
         if (check_user_exists_pv(private_key))
             return users[pv_users_map[private_key]];
@@ -97,26 +99,35 @@ contract SpotCoin {
      * Get user balance by private key.
      */
     function get_balance(string memory public_key) public view returns (int) {
-        if (check_user_exists_pv(public_key)) {
-            User memory user = get_user_by_public_key(public_key);
-            return user.balance;
+        // get user balance by public key
+        if (check_user_exists_pb(public_key))
+            return get_user_by_public_key(public_key).balance;
+        else revert ("User does not exist");
+    }
+    /**
+     * Get user balance by private key.
+     */
+    function get_transactions_for_user(string memory private_key) public view returns (Transaction[] memory) {
+        // get all the transactions for the user by private key
+        if (check_user_exists_pv(private_key)) {
+            User memory user = get_user_by_private_key(private_key);
+            Transaction[] memory transactions = new Transaction[](user.transaction_ids.length);
+            for (uint i = 0; i < user.transaction_ids.length; i++) {
+                transactions[i] = global_transactions[user.transaction_ids[i]];
+            }
+            return transactions;
         }
         else revert ("User does not exist");
     }
-
     /**
      * Create a new user.
      */
     function add_new_user(string memory private_key, string memory public_key) public {
         if (!check_user_unique(private_key, public_key)) {
-            uint index = users.length;
-
-            User memory new_user = User(index, private_key, public_key, 0, new uint[](0), new uint[](0), new uint[](0));
-
-            pv_users_map[private_key] = index;
-            pb_users_map[public_key] = index;
-
-            users.push(new_user);
+            User memory user = User(users.length, private_key, public_key, 0, new uint[](0), new uint[](0), new uint[](0));
+            users.push(user);
+            pv_users_map[private_key] = users.length;
+            pb_users_map[public_key] = users.length;
         } 
         else revert ("User already exists");
     }
@@ -157,11 +168,11 @@ contract SpotCoin {
             // add the transaction to both users
             Transaction memory transaction = Transaction(block.timestamp, sender.public_key, receiver.public_key, amount, TransactionType.sent);
             global_transactions[sender.transaction_ids.length] = transaction;
-            sender.transaction_ids.push(sender.transaction_ids.length);
+            sender.transaction_ids[sender.transaction_ids.length] = sender.transaction_ids.length;
 
             transaction = Transaction(block.timestamp, sender.public_key, receiver.public_key, amount, TransactionType.received);
             global_transactions[receiver.transaction_ids.length] = transaction;
-            receiver.transaction_ids.push(receiver.transaction_ids.length);
+            receiver.transaction_ids[receiver.transaction_ids.length] = receiver.transaction_ids.length;
 
             // store the modified objects
             users[pb_users_map[sender_pub]] = sender;
@@ -201,7 +212,7 @@ contract SpotCoin {
                 // add y=transaction to user
                 Transaction memory transaction = Transaction(block.timestamp, user.public_key, "the SPOT Creator", base_price, TransactionType.spot_creation);
                 global_transactions[user.transaction_ids.length] = transaction;
-                user.transaction_ids.push(user.transaction_ids.length);
+                user.transaction_ids[user.transaction_ids.length] = user.transaction_ids.length;
 
                 // store the modified objects
                 users[pv_users_map[private_key]] = user;
@@ -210,6 +221,61 @@ contract SpotCoin {
         }
         else revert ("Spot already exists");
     }
+    //----------------------------------------------------------------------------------------------------------------//
+    /**
+     * Get all the spots for a user
+     */
+    function get_all_spots_owned_by_user(string memory private_key) public view returns (Spot[] memory) {
+        User memory user = get_user_by_private_key(private_key);
+        Spot[] memory spots = new Spot[](user.owned_spots.length);
+        for (uint i = 0; i < user.owned_spots.length; i++) {
+            spots[i] = global_spots[user.owned_spots[i]];
+        }
+        return spots;
+    }
+    //----------------------------------------------------------------------------------------------------------------//
+    /**
+     * Get a spot by index
+     */
+    function get_spot(uint index) public view returns (Spot memory) {
+        return global_spots[index];
+    }
+    //----------------------------------------------------------------------------------------------------------------//
+    function buy_spot(uint index, string memory private_key) public {
+        User memory user = get_user_by_private_key(private_key);
+        Spot storage spot = global_spots[index];
+        if (user.balance > spot.current_price)
+        {
+            // adjust the balance of both users
+            user.balance -= spot.current_price;
+            User memory old_owner = get_user_by_public_key(spot.current_owner);
+            old_owner.balance += spot.current_price;
+
+            // add the transaction to both users
+            Transaction memory transaction = Transaction(block.timestamp, user.public_key, old_owner.public_key, spot.current_price, TransactionType.spot_purchase);
+            global_transactions[user.transaction_ids.length] = transaction;
+            user.transaction_ids[user.transaction_ids.length] = user.transaction_ids.length;
+
+            transaction = Transaction(block.timestamp, user.public_key, old_owner.public_key, spot.current_price, TransactionType.spot_sale);
+            global_transactions[old_owner.transaction_ids.length] = transaction;
+            old_owner.transaction_ids[old_owner.transaction_ids.length] = old_owner.transaction_ids.length;
+
+            // store the modified objects
+            users[pv_users_map[private_key]] = user;
+            users[pb_users_map[spot.current_owner]] = old_owner;
+
+            // adjust the spot
+            spot.current_owner = user.public_key;
+            spot.owners_chain.push(user.public_key);
+            spot.current_price = spot.base_price;
+            spot.last_reset = block.timestamp;
+
+            // store the modified spot
+            global_spots[index] = spot;
+        }
+        else revert ("Not enough money in account");
+    }
+    //----------------------------------------------------------------------------------------------------------------//
     //----------------------------------------------------------------------------------------------------------------//
     // spot pool-related functions
     function get_spot_amount() public view returns (int, uint) {
