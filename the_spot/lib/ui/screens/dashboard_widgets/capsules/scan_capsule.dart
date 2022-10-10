46c492904f61c0a916bc7337f74b7bce38ceed5b
MrHup <hup.hubspace@gmail.com>
2022-10-10 22:34:10 +0300

    Initialize NFC functionalities

--- the_spot/lib/ui/screens/dashboard_widgets/capsules/scan_capsule.dart
+++ the_spot/lib/ui/screens/dashboard_widgets/capsules/scan_capsule.dart
@@ -1,13 +1,63 @@
+import 'dart:convert';
+import 'package:ndef/ndef.dart' as ndef;
+
 import 'package:flutter/material.dart';
 import 'package:the_spot/config/custom_extensions.dart';
 import 'package:the_spot/config/theme_data.dart';
 import 'package:the_spot/ui/screens/dashboard_widgets/dashboard_drip.dart';
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 
 class ScanCapsule extends StatelessWidget {
   const ScanCapsule({super.key});
 
+  void enableNFC(BuildContext context) async {
+    try {
+      var availability = await FlutterNfcKit.nfcAvailability;
+      if (availability != NFCAvailability.available) {
+        // oh-no
+      }
+
+// timeout only works on Android, while the latter two messages are only for iOS
+      var tag = await FlutterNfcKit.poll(
+          timeout: Duration(seconds: 10),
+          iosMultipleTagMessage: "Multiple tags found!",
+          iosAlertMessage: "Scan your tag");
+
+      print(jsonEncode(tag));
+
+      // read NDEF records if available
+      if (tag.ndefAvailable != null) {
+        print("NDEF available");
+        if (tag.ndefAvailable!) {
+          for (var record
+              in await FlutterNfcKit.readNDEFRecords(cached: false)) {
+            print(record.toString());
+          }
+
+          for (var record
+              in await FlutterNfcKit.readNDEFRawRecords(cached: false)) {
+            print(jsonEncode(record).toString());
+          }
+        }
+
+        /// raw NDEF records (data in hex string)
+        /// `{identifier: "", payload: "00010203", type: "0001", typeNameFormat: "nfcWellKnown"}`
+        for (var record
+            in await FlutterNfcKit.readNDEFRawRecords(cached: false)) {
+          print(jsonEncode(record).toString());
+        }
+      }
+
+      print("finish up");
+      await FlutterNfcKit.finish();
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("On build");
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       body: Stack(children: [
@@ -22,7 +72,10 @@ class ScanCapsule extends StatelessWidget {
                     maxWidth: MediaQuery.of(context).size.width * 0.8,
                     maxHeight: 50,
                   ),
-                  child: const Image(image: AssetImage('assets/img/logo.png')),
+                  child: GestureDetector(
+                      onTap: () => enableNFC(context),
+                      child: const Image(
+                          image: AssetImage('assets/img/logo.png'))),
                 ).withPadding(8),
               ],
             ),
