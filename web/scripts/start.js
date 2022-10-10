61afa0a68d2b2e1feb2fad836161931f877f17d6
Diana Anton <diana.anton00@e-uvt.ro>
2022-10-10 22:50:10 +0300

    Working firebase config

--- web/scripts/start.js
+++ web/scripts/start.js
@@ -1,9 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const initializeApp = require("firebase/app").initializeApp;
+// const getAnalytics = require("firebase/analytics").getAnalytics;
 const concurrently = require("concurrently");
 const upath = require("upath");
 
@@ -26,7 +28,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 concurrently(
     [
